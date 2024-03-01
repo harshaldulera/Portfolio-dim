9bca558b4a56ad2706e481bb72494ffbd0937e50
StevyxD <kumarmayank310@gmail.com>
2024-03-01 18:19:21 +0530

    project addition

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -26,8 +26,8 @@ const Project = () => {
           <p className="text-gray-300 font-bold hover:text-white duration-500 ">
             Utilized effective coding techniques and debugging practices during
             development to effectively manage website bugs and errors Developed
-            an expense tracking web application using React.js, . Utilized
-            effective coding techniques and debugging practices during
+            an expense tracking web application using React.js. <br />
+            Utilized effective coding techniques and debugging practices during
             development to effectively manage website bugs and errors.
           </p>
           <p className="text-gray-300 font-bold hover:text-white duration-500 ">
@@ -51,17 +51,10 @@ const Project = () => {
           <p className="text-gray-300 font-bold hover:text-white duration-500 ">
             Project aimed towards helping people track their daily exercises. •
           </p>
-
-          <p className="text-gray-300 font-bold hover:text-white duration-500 ">
-            It helped me learn new REACT concepts such as useState hook.
-          </p>
           <p className="text-gray-300 font-bold hover:text-white duration-500 ">
             This project helped me learn CRUD operations in React framework.
           </p>
-          <p className="text-gray-300 font-bold hover:text-white duration-500 ">
-            An individual project where I used React framework fundamentals like
-            useState hook and dynamic styling methods to develop this website.
-          </p>
+          <br />
           <p className="text-gray-300 font-bold hover:text-white duration-500 ">
             Developed and deployed an exercise tracking web app which enabled
             users to log and track their exercise activities, with real- time
@@ -70,32 +63,24 @@ const Project = () => {
         </div>
         <div className="w-[500px] flex-col p-10 bg-slate-600  rounded-xl my-8 mx-2 hover:scale-105 cursor-pointer duration-500 ">
           <h1 className="text-2xl hover:scale-105 flex justify-center font-bold text-gray-300 duration-500">
-            React Tailwind webOrder
+            Budget Buddy
           </h1>
           <h1 className="hover:scale-105 font-bold text-gray-500 duration-500">
-            July 2023 - July 2023
+            February 2024
           </h1>
 
           <p className="text-gray-300 font-bold hover:text-white duration-500 ">
-            Created this website for learning Tailwind framework and it also
-            helped me in revising CSS and React concepts.
+            HTML CSS ,React js and Tailwind UI Integrated AI technology into the
+            website for enhanced budget calculations.
           </p>
 
           <p className="text-gray-300 font-bold hover:text-white duration-500 ">
-            Creating this website helped me learn how to design beautiful
-            responsive webpages and use React Router Dom.
+            Implemented features such as a chatbot, AI budget calculator, and
+            expense trackingfunctionality.
           </p>
           <p className="text-gray-300 font-bold hover:text-white duration-500 ">
             Utilized Tailwind framework, HTML, and CSS to develop a fully
             responsive website, optimized for user engagement.
-          </p>
-          <p className="text-gray-300 font-bold hover:text-white duration-500 ">
-            Used concepts like grids, flex, etc. and used used effects like
-            hover and active to create a user friendly webpage.
-          </p>
-          <p className="text-gray-300 font-bold hover:text-white duration-500 ">
-            I used React Router Dom to change navigate webpages without
-            reloading website which helped improve e ciency of website.
           </p>
         </div>
       </div>
