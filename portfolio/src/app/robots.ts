6677ea1372c88cce972953618734c4437a923cfc
Harshal Dulera <harshaldulera82@gmail.com>
2025-09-11 12:31:26 +0530

    fix: robots.txt and baseUrl

--- portfolio/src/app/robots.ts
+++ portfolio/src/app/robots.ts
@@ -1,13 +1,18 @@
 import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "http://harshal0x01.vercel.app";
 
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: ["/", "/contact"],
+        disallow: [
+          "/experience",
+        ],
+      },
+    ],
     sitemap: `${baseUrl}/sitemap.xml`,
   };
 }
